d56b33fdbb7ff21d09661dace8d861bf14ae27d1
Dhrumil Choksi <41354644+choksidhrumil2000@users.noreply.github.com>
2020-10-28 11:39:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
 	<title>My Website</title>
 </head>
 <body>
-	<h1>This is Website Deployment with App Engine</h1>
+	<h1><center>This is Website Deployment with Heroku</center></h1>
 	<b><center>I am 17CE018-Dhrumil Choksi</center></b>
 
 </body>
 </html>
 <?php
 
-include_once 'index.html';
+//include_once 'index.html';
 //echo 'Hello World!!';
 ?>
