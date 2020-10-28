6005543876e26c5489dfb378fc9696a0eb6abc7e
Dhrumil Choksi <41354644+choksidhrumil2000@users.noreply.github.com>
2020-10-28 11:37:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
-<h1> hello world </h1>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Website</title>
+</head>
+<body>
+	<h1>This is Website Deployment with App Engine</h1>
+	<b><center>I am 17CE018-Dhrumil Choksi</center></b>
 
+</body>
+</html>
 <?php
 
 include_once 'index.html';
