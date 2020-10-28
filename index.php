c965499df1de841d2cc2b381049f590764bad1cf
Dhrumil Choksi <41354644+choksidhrumil2000@users.noreply.github.com>
2020-10-28 11:49:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <body>
 	<h1><center>This is Website Deployment with Heroku</center></h1>
 	<b><center>I am 17CE018-Dhrumil Choksi</center></b>
-<P> So, this is the marketing Page of Amazon Products You cna visit whatever you want from Below link to amazon directly where you can search whatever product you want</p>
+	<P> So, this is the marketing Page of <b>Amazon Products</b> You cna visit whatever you want from Below link to amazon directly where you can search whatever product you want</p>
 	<a href="https://www.amazon.in/">Click here To Visit Amazon!!</a>
 
 </body>
